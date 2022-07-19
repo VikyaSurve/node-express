fa576fbea1b326014354e87caeb9998573d8d2a8
Vikas Surve <VikyaSurve@users.noreply.github.com>
2022-07-19 19:23:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from the Vikas!');
+  res.send('Hello from the Server!');
 });
 
 app.get('/health', (req, res) => {
